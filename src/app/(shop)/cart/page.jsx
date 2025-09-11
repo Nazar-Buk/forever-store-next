4d05cd343c03bff9016811240888646a0337a061
Nazar Buk <nazarbuk27@gmail.com>
2025-09-12 01:55:31 +0300

    Add loader to Cart page

--- src/app/(shop)/cart/page.jsx
+++ src/app/(shop)/cart/page.jsx
@@ -10,6 +10,7 @@ import { ShopContext } from "@/context/ShopContext";
 import { assets } from "../../../../public/assets/assets";
 import Title from "@/components/Title";
 import CartTotal from "@/components/CartTotal";
+import Loader from "@/components/Loader";
 
 const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
 
@@ -24,6 +25,10 @@ const Cart = () => {
     removeAllCartProducts,
     setCheckedSize,
   } = useContext(ShopContext);
+
+  const [isLoadingState, setIsLoadingState] = useState({
+    isProductsLoading: true,
+  });
 
   const [cartData, setCartData] = useState([]);
   const [allCartProducts, setAllCartProducts] = useState([]);
@@ -86,148 +91,169 @@ const Cart = () => {
 
   // ⚡️ завантажуємо всі продукти, коли змінюється cartData
   useEffect(() => {
-    if (!cartData.length) return;
+    setIsLoadingState((prev) => ({ ...prev, isProductsLoading: true }));
+
+    if (!cartData.length) {
+      setIsLoadingState((prev) => ({ ...prev, isProductsLoading: false }));
+      return;
+    }
 
     const fetchAllCartProducts = async () => {
-      const cartProducts = await getAllCartProducts();
-      setAllCartProducts(cartProducts);
+      try {
+        const cartProducts = await getAllCartProducts();
+        setAllCartProducts(cartProducts);
+        setIsLoadingState((prev) => ({ ...prev, isProductsLoading: false }));
+      } catch (error) {
+        console.log(error, "error");
+        setIsLoadingState((prev) => ({ ...prev, isProductsLoading: false }));
+      }
     };
 
     fetchAllCartProducts();
   }, [cartData.length]);
 
+  const isLoading = isLoadingState.isProductsLoading;
+
   return (
-    <section className="cart-page">
-      {cartData.length ? (
-        <div className="cart__container">
-          <div className="cart__body">
-            <div className="wrap-title-remove-all">
-              <Title text1="Your " text2="CART" />
-              <button onClick={() => removeAllCartProducts()}>
-                Remove all
-              </button>
-            </div>
-
-            {cartData.map((item, index) => {
-              const productData = allCartProducts?.find(
-                (product) => product._id === item._id
-              );
-
-              // const checkedSize = item.size;
-              return (
-                <div key={index} className="cart__product">
-                  <Link
-                    href={`/product/${item._id}`}
-                    onClick={() => setCheckedSize(item?.size)}
-                    className="wrap-image-details"
-                  >
-                    <div className="wrap-cart__product-img">
-                      <img
-                        src={productData?.images[0].url}
-                        alt="product image"
-                      />
-                    </div>
-                    <div className="cart__product-details">
-                      <p className="details__name">{productData?.name}</p>
-                      <div className="wrap-details__price-size">
-                        <p className="details__price">
-                          {currency} {productData?.price * item?.quantity}
-                        </p>
-                        <div className="details__size">{item?.size}</div>
-                      </div>
-                    </div>
-                  </Link>
-                  <div className="wrap-qty-trash">
-                    <div className="wrap-qty-input">
-                      <label htmlFor={`qty-${index}`}>Quantity:</label>
-                      <div className="wrap-btns-input">
-                        <div
-                          onClick={() => {
-                            const quantity =
-                              item.quantity - 1 > 1 ? item.quantity - 1 : 1;
-
-                            updateQuantity(item._id, item.size, quantity);
-                          }}
-                          className="decrease"
-                        >
-                          <span>-</span>
-                        </div>
-                        <input
-                          id={`qty-${index}`}
-                          className="qty-input"
-                          type="number"
-                          min={1}
-                          readOnly={true}
-                          // defaultValue={item.quantity}
-                          value={item.quantity}
-                          // onChange={(e) => {
-                          //   const { value } = e.target;
-                          //   value === "" || value === 0
-                          //     ? null
-                          //     : updateQuantity(
-                          //         item._id,
-                          //         item.size,
-                          //         Number(value)
-                          //       );
-                          // }}
-                        />
-                        <div
-                          onClick={() => {
-                            const quantity = item.quantity + 1;
-
-                            updateQuantity(item._id, item.size, quantity);
-                          }}
-                          className="increase"
-                        >
-                          +
-                        </div>
-                      </div>
-                    </div>
-                    <div className="wrap-trash-icon">
-                      <img
-                        onClick={() => updateQuantity(item._id, item.size, 0)}
-                        src={assets.bin_icon}
-                        alt="delete icon"
-                      />
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-          <div className="cart-total-box">
-            <CartTotal allCartProducts={allCartProducts} />
-            <button onClick={() => router.push("/place-order")}>
-              Proceed to checkout
-            </button>
-          </div>
-        </div>
+    <>
+      {isLoading ? (
+        <Loader />
       ) : (
-        <div className="empty-cart-box">
-          <h1>Your Cart Is Empty !</h1>
-          <div className="wrap-empty-img">
-            <img src={assets.empty_cart} alt="empty card icon" />
-          </div>
-          <Link
-            href={{
-              pathname: "/collection",
-              query: {
-                page: 1,
-                limit: 10,
-                category: "",
-                subCategory: "",
-                priceFrom: "",
-                priceTo: "",
-                sort: "date_new",
-                search: "",
-              },
-            }}
-          >
-            <button>Go To Product Page</button>
-          </Link>
-        </div>
+        <section className="cart-page">
+          {cartData.length ? (
+            <div className="cart__container">
+              <div className="cart__body">
+                <div className="wrap-title-remove-all">
+                  <Title text1="Your " text2="CART" />
+                  <button onClick={() => removeAllCartProducts()}>
+                    Remove all
+                  </button>
+                </div>
+
+                {cartData.map((item, index) => {
+                  const productData = allCartProducts?.find(
+                    (product) => product._id === item._id
+                  );
+
+                  // const checkedSize = item.size;
+                  return (
+                    <div key={index} className="cart__product">
+                      <Link
+                        href={`/product/${item._id}`}
+                        onClick={() => setCheckedSize(item?.size)}
+                        className="wrap-image-details"
+                      >
+                        <div className="wrap-cart__product-img">
+                          <img
+                            src={productData?.images[0].url}
+                            alt="product image"
+                          />
+                        </div>
+                        <div className="cart__product-details">
+                          <p className="details__name">{productData?.name}</p>
+                          <div className="wrap-details__price-size">
+                            <p className="details__price">
+                              {currency} {productData?.price * item?.quantity}
+                            </p>
+                            <div className="details__size">{item?.size}</div>
+                          </div>
+                        </div>
+                      </Link>
+                      <div className="wrap-qty-trash">
+                        <div className="wrap-qty-input">
+                          <label htmlFor={`qty-${index}`}>Quantity:</label>
+                          <div className="wrap-btns-input">
+                            <div
+                              onClick={() => {
+                                const quantity =
+                                  item.quantity - 1 > 1 ? item.quantity - 1 : 1;
+
+                                updateQuantity(item._id, item.size, quantity);
+                              }}
+                              className="decrease"
+                            >
+                              <span>-</span>
+                            </div>
+                            <input
+                              id={`qty-${index}`}
+                              className="qty-input"
+                              type="number"
+                              min={1}
+                              readOnly={true}
+                              // defaultValue={item.quantity}
+                              value={item.quantity}
+                              // onChange={(e) => {
+                              //   const { value } = e.target;
+                              //   value === "" || value === 0
+                              //     ? null
+                              //     : updateQuantity(
+                              //         item._id,
+                              //         item.size,
+                              //         Number(value)
+                              //       );
+                              // }}
+                            />
+                            <div
+                              onClick={() => {
+                                const quantity = item.quantity + 1;
+
+                                updateQuantity(item._id, item.size, quantity);
+                              }}
+                              className="increase"
+                            >
+                              +
+                            </div>
+                          </div>
+                        </div>
+                        <div className="wrap-trash-icon">
+                          <img
+                            onClick={() =>
+                              updateQuantity(item._id, item.size, 0)
+                            }
+                            src={assets.bin_icon}
+                            alt="delete icon"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="cart-total-box">
+                <CartTotal allCartProducts={allCartProducts} />
+                <button onClick={() => router.push("/place-order")}>
+                  Proceed to checkout
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div className="empty-cart-box">
+              <h1>Your Cart Is Empty !</h1>
+              <div className="wrap-empty-img">
+                <img src={assets.empty_cart} alt="empty card icon" />
+              </div>
+              <Link
+                href={{
+                  pathname: "/collection",
+                  query: {
+                    page: 1,
+                    limit: 10,
+                    category: "",
+                    subCategory: "",
+                    priceFrom: "",
+                    priceTo: "",
+                    sort: "date_new",
+                    search: "",
+                  },
+                }}
+              >
+                <button>Go To Product Page</button>
+              </Link>
+            </div>
+          )}
+        </section>
       )}
-    </section>
+    </>
   );
 };
 
