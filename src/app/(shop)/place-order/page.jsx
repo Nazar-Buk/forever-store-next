d827d4d929d3875d0e8700457fad3cc036ebd3a4
Nazar Buk <nazarbuk27@gmail.com>
2025-09-12 22:42:57 +0300

    Rebuild Place order page structure

--- src/app/(shop)/place-order/page.jsx
+++ src/app/(shop)/place-order/page.jsx
@@ -75,178 +75,181 @@ const PlaceOrder = () => {
   return (
     <section className="payment-page">
       <div className="payment__container">
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          noValidate
-          className="payment__body"
-        >
-          <div className="payment__delivery-info">
-            <Title text1="Delivery " text2="Information" />
-            <div className="delivery-info__form-fields">
-              <div className="wrap-short-fields">
+        <div className="wrap__title_and_payment__body">
+          <Title text1="Delivery " text2="Information" />
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            noValidate
+            className="payment__body"
+          >
+            <div className="payment__delivery-info">
+              <div className="delivery-info__form-fields">
+                <div className="wrap-short-fields">
+                  <div className="wrap-input">
+                    <input
+                      type="text"
+                      id="first-name"
+                      className="delivery-info__field"
+                      placeholder="First name"
+                      {...register("firstName")}
+                    />
+                    <p className="error">{errors.firstName?.message}</p>
+                  </div>
+
+                  <div className="wrap-input">
+                    <input
+                      type="text"
+                      id="last-name"
+                      className="delivery-info__field"
+                      placeholder="Last name"
+                      {...register("lastName")}
+                    />
+                    <p className="error">{errors.lastName?.message}</p>
+                  </div>
+                </div>
                 <div className="wrap-input">
                   <input
-                    type="text"
-                    id="first-name"
-                    className="delivery-info__field"
-                    placeholder="First name"
-                    {...register("firstName")}
+                    type="email"
+                    id="email"
+                    className="delivery-info__field long-field"
+                    placeholder="Email address"
+                    {...register("email")}
                   />
-                  <p className="error">{errors.firstName?.message}</p>
+                  <p className="error">{errors.email?.message}</p>
                 </div>
 
                 <div className="wrap-input">
                   <input
                     type="text"
-                    id="last-name"
-                    className="delivery-info__field"
-                    placeholder="Last name"
-                    {...register("lastName")}
+                    id="street"
+                    className="delivery-info__field long-field"
+                    placeholder="Street"
+                    {...register("street")}
                   />
-                  <p className="error">{errors.lastName?.message}</p>
+                  <p className="error">{errors.street?.message}</p>
                 </div>
-              </div>
-              <div className="wrap-input">
-                <input
-                  type="email"
-                  id="email"
-                  className="delivery-info__field long-field"
-                  placeholder="Email address"
-                  {...register("email")}
-                />
-                <p className="error">{errors.email?.message}</p>
-              </div>
 
-              <div className="wrap-input">
-                <input
-                  type="text"
-                  id="street"
-                  className="delivery-info__field long-field"
-                  placeholder="Street"
-                  {...register("street")}
-                />
-                <p className="error">{errors.street?.message}</p>
-              </div>
+                <div className="wrap-short-fields">
+                  <div className="wrap-input">
+                    <input
+                      type="text"
+                      id="city"
+                      className="delivery-info__field"
+                      placeholder="City"
+                      {...register("city")}
+                    />
+                    <p className="error">{errors.city?.message}</p>
+                  </div>
 
-              <div className="wrap-short-fields">
+                  <div className="wrap-input">
+                    <input
+                      type="text"
+                      id="state"
+                      className="delivery-info__field"
+                      placeholder="State"
+                      {...register("state")}
+                    />
+                    <p className="error">{errors.state?.message}</p>
+                  </div>
+                </div>
+                <div className="wrap-short-fields">
+                  <div className="wrap-input">
+                    <input
+                      type="number"
+                      id="zip-code"
+                      className="delivery-info__field"
+                      placeholder="Zip code"
+                      {...register("zip_code")}
+                    />
+                    <p className="error">{errors.zip_code?.message}</p>
+                  </div>
+
+                  <div className="wrap-input">
+                    <input
+                      type="text"
+                      id="country"
+                      className="delivery-info__field"
+                      placeholder="Country"
+                      {...register("country")}
+                    />
+                    <p className="error">{errors.country?.message}</p>
+                  </div>
+                </div>
                 <div className="wrap-input">
                   <input
                     type="text"
-                    id="city"
-                    className="delivery-info__field"
-                    placeholder="City"
-                    {...register("city")}
+                    id="phone"
+                    className="delivery-info__field long-field"
+                    placeholder="Phone"
+                    {...register("phone")}
                   />
-                  <p className="error">{errors.city?.message}</p>
+                  <p className="error">{errors.phone?.message}</p>
                 </div>
-
-                <div className="wrap-input">
-                  <input
-                    type="text"
-                    id="state"
-                    className="delivery-info__field"
-                    placeholder="State"
-                    {...register("state")}
-                  />
-                  <p className="error">{errors.state?.message}</p>
-                </div>
-              </div>
-              <div className="wrap-short-fields">
-                <div className="wrap-input">
-                  <input
-                    type="number"
-                    id="zip-code"
-                    className="delivery-info__field"
-                    placeholder="Zip code"
-                    {...register("zip_code")}
-                  />
-                  <p className="error">{errors.zip_code?.message}</p>
-                </div>
-
-                <div className="wrap-input">
-                  <input
-                    type="text"
-                    id="country"
-                    className="delivery-info__field"
-                    placeholder="Country"
-                    {...register("country")}
-                  />
-                  <p className="error">{errors.country?.message}</p>
-                </div>
-              </div>
-              <div className="wrap-input">
-                <input
-                  type="text"
-                  id="phone"
-                  className="delivery-info__field long-field"
-                  placeholder="Phone"
-                  {...register("phone")}
-                />
-                <p className="error">{errors.phone?.message}</p>
               </div>
             </div>
-          </div>
-          <div className="payment__methods-and-cart-totals">
-            <CartTotal />
-            <Title text1="Payment " text2="Method" />
-            <div className="payments-group">
-              <div className="payment-box">
-                <div className="wrap-radio-input">
-                  <input
-                    className="stripe"
-                    type="radio"
-                    {...register("payment_method")}
-                    value="stripe"
-                    id="stripe"
-                  />
-                </div>
-                <label className="stripe" htmlFor="stripe">
-                  <img src={assets.stripe_logo} alt="stripe" />
-                </label>
+          </form>
+          {/* <DevTool control={control} /> */}
+        </div>
+
+        <div className="payment__methods-and-cart-totals">
+          <CartTotal />
+          <Title text1="Payment " text2="Method" />
+          <div className="payments-group">
+            <div className="payment-box">
+              <div className="wrap-radio-input">
+                <input
+                  className="stripe"
+                  type="radio"
+                  {...register("payment_method")}
+                  value="stripe"
+                  id="stripe"
+                />
               </div>
-              <div className="payment-box">
-                <div className="wrap-radio-input">
-                  <input
-                    className="razorpay"
-                    type="radio"
-                    {...register("payment_method")}
-                    value="razorpay"
-                    id="razorpay"
-                  />
-                </div>
-                <label className="razorpay" htmlFor="razorpay">
-                  <img src={assets.razorpay_logo} alt="razorpay" />
-                </label>
-              </div>
-              <div className="payment-box">
-                <div className="wrap-radio-input">
-                  <input
-                    className="cod"
-                    type="radio"
-                    {...register("payment_method")}
-                    value="cod"
-                    id="cod"
-                  />
-                </div>
-                <label className="cod" htmlFor="cod">
-                  CASH ON DELIVERY
-                </label>
-              </div>
+              <label className="stripe" htmlFor="stripe">
+                <img src={assets.stripe_logo} alt="stripe" />
+              </label>
             </div>
-            <button
-              // onClick={() => {
-              //   if (isDirty && isValid && !isSubmitting) {
-              //     router.push("/orders");
-              //   }
-              // }}
-              disabled={isSubmitting}
-              type="submit"
-            >
-              Place payment
-            </button>
+            <div className="payment-box">
+              <div className="wrap-radio-input">
+                <input
+                  className="razorpay"
+                  type="radio"
+                  {...register("payment_method")}
+                  value="razorpay"
+                  id="razorpay"
+                />
+              </div>
+              <label className="razorpay" htmlFor="razorpay">
+                <img src={assets.razorpay_logo} alt="razorpay" />
+              </label>
+            </div>
+            <div className="payment-box">
+              <div className="wrap-radio-input">
+                <input
+                  className="cod"
+                  type="radio"
+                  {...register("payment_method")}
+                  value="cod"
+                  id="cod"
+                />
+              </div>
+              <label className="cod" htmlFor="cod">
+                CASH ON DELIVERY
+              </label>
+            </div>
           </div>
-        </form>
-        {/* <DevTool control={control} /> */}
+          <button
+            // onClick={() => {
+            //   if (isDirty && isValid && !isSubmitting) {
+            //     router.push("/orders");
+            //   }
+            // }}
+            disabled={isSubmitting}
+            type="submit"
+          >
+            Place payment
+          </button>
+        </div>
       </div>
     </section>
   );
