59caeb4d99793562f2de0c037c46432f9046ab5d
Nazar Buk <nazarbuk27@gmail.com>
2025-09-13 00:10:43 +0300

    Refactor of Place order page
    Move "COD" form to separate component.
    Change title and form according to checked payment method.

--- src/app/(shop)/place-order/page.jsx
+++ src/app/(shop)/place-order/page.jsx
@@ -9,6 +9,7 @@ import * as yup from "yup";
 
 import Title from "@/components/Title";
 import CartTotal from "@/components/CartTotal";
+import CODForm from "@/components/forms/CODForm";
 import { assets } from "../../../../public/assets/assets";
 
 const schema = yup.object({
@@ -40,6 +41,12 @@ const schema = yup.object({
     .required("This field is required!"),
 });
 
+const paymentMethodTitles = {
+  stripe: { text1: "Stripe ", text2: "Information" },
+  razorpay: { text1: "Razorpay ", text2: "Information" },
+  cod: { text1: "Delivery ", text2: "Information" },
+};
+
 const PlaceOrder = () => {
   // const navigate = useNavigate();
   const router = useRouter();
@@ -60,9 +67,12 @@ const PlaceOrder = () => {
     resolver: yupResolver(schema),
   });
 
-  const { register, control, handleSubmit, formState } = form;
+  const { register, control, handleSubmit, formState, watch, clearErrors } =
+    form;
   const { errors, isDirty, isValid, isSubmitting } = formState;
   console.log(errors, "errors");
+
+  const checkPaymentMethodType = watch("payment_method");
 
   const onSubmit = (data) => {
     console.log(data, "Form Submitted!");
@@ -72,122 +82,32 @@ const PlaceOrder = () => {
     }
   };
 
+  const handlePay = () => {
+    if (checkPaymentMethodType === "cod") {
+      handleSubmit(onSubmit)(); //() треба викликати функцію
+    } else {
+      clearErrors();
+      console.log("hahahahhahah");
+    }
+  };
+
   return (
     <section className="payment-page">
       <div className="payment__container">
         <div className="wrap__title_and_payment__body">
-          <Title text1="Delivery " text2="Information" />
-          <form
-            onSubmit={handleSubmit(onSubmit)}
-            noValidate
-            className="payment__body"
-          >
-            <div className="payment__delivery-info">
-              <div className="delivery-info__form-fields">
-                <div className="wrap-short-fields">
-                  <div className="wrap-input">
-                    <input
-                      type="text"
-                      id="first-name"
-                      className="delivery-info__field"
-                      placeholder="First name"
-                      {...register("firstName")}
-                    />
-                    <p className="error">{errors.firstName?.message}</p>
-                  </div>
+          <Title
+            text1={paymentMethodTitles[checkPaymentMethodType].text1}
+            text2={paymentMethodTitles[checkPaymentMethodType].text2}
+          />
+          {checkPaymentMethodType === "cod" && (
+            <CODForm
+              handleSubmit={handleSubmit}
+              onSubmit={onSubmit}
+              register={register}
+              errors={errors}
+            />
+          )}
 
-                  <div className="wrap-input">
-                    <input
-                      type="text"
-                      id="last-name"
-                      className="delivery-info__field"
-                      placeholder="Last name"
-                      {...register("lastName")}
-                    />
-                    <p className="error">{errors.lastName?.message}</p>
-                  </div>
-                </div>
-                <div className="wrap-input">
-                  <input
-                    type="email"
-                    id="email"
-                    className="delivery-info__field long-field"
-                    placeholder="Email address"
-                    {...register("email")}
-                  />
-                  <p className="error">{errors.email?.message}</p>
-                </div>
-
-                <div className="wrap-input">
-                  <input
-                    type="text"
-                    id="street"
-                    className="delivery-info__field long-field"
-                    placeholder="Street"
-                    {...register("street")}
-                  />
-                  <p className="error">{errors.street?.message}</p>
-                </div>
-
-                <div className="wrap-short-fields">
-                  <div className="wrap-input">
-                    <input
-                      type="text"
-                      id="city"
-                      className="delivery-info__field"
-                      placeholder="City"
-                      {...register("city")}
-                    />
-                    <p className="error">{errors.city?.message}</p>
-                  </div>
-
-                  <div className="wrap-input">
-                    <input
-                      type="text"
-                      id="state"
-                      className="delivery-info__field"
-                      placeholder="State"
-                      {...register("state")}
-                    />
-                    <p className="error">{errors.state?.message}</p>
-                  </div>
-                </div>
-                <div className="wrap-short-fields">
-                  <div className="wrap-input">
-                    <input
-                      type="number"
-                      id="zip-code"
-                      className="delivery-info__field"
-                      placeholder="Zip code"
-                      {...register("zip_code")}
-                    />
-                    <p className="error">{errors.zip_code?.message}</p>
-                  </div>
-
-                  <div className="wrap-input">
-                    <input
-                      type="text"
-                      id="country"
-                      className="delivery-info__field"
-                      placeholder="Country"
-                      {...register("country")}
-                    />
-                    <p className="error">{errors.country?.message}</p>
-                  </div>
-                </div>
-                <div className="wrap-input">
-                  <input
-                    type="text"
-                    id="phone"
-                    className="delivery-info__field long-field"
-                    placeholder="Phone"
-                    {...register("phone")}
-                  />
-                  <p className="error">{errors.phone?.message}</p>
-                </div>
-              </div>
-            </div>
-          </form>
           {/* <DevTool control={control} /> */}
         </div>
 
@@ -245,7 +165,9 @@ const PlaceOrder = () => {
             //   }
             // }}
             disabled={isSubmitting}
-            type="submit"
+            type="button" // навмисно написав button, бо кнопка не у формі, submit буде шукати її і не буде працювати
+            // onClick={() => handleSubmit(onSubmit)()} //() треба викликати функцію
+            onClick={handlePay}
           >
             Place payment
           </button>
