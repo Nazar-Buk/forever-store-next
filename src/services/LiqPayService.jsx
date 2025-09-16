56c552bcb6a668c57ee78fbadf886d17268edcbb
Nazar Buk <nazarbuk27@gmail.com>
2025-09-16 21:14:31 +0300

    Add go to products button to LiqPay success payment
    Add styles for button to LiqPay success payment

--- src/services/LiqPayService.jsx
+++ src/services/LiqPayService.jsx
@@ -1,11 +1,14 @@
 "use client";
 
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
+import Link from "next/link";
 
 import { ShopContext } from "@/context/ShopContext";
 
 const LiqPayService = ({ setLoadingState }) => {
   const { backendUrl, getCartAmount } = useContext(ShopContext);
+
+  const [isButtonShown, setIsButtonShown] = useState(false);
 
   const downloadLiqPayForm = async (signal) => {
     try {
@@ -53,9 +56,12 @@ const LiqPayService = ({ setLoadingState }) => {
       mode: "embed",
       language: "uk",
     })
-      .on("liqpay.callback", (result) =>
-        console.log("liqpay.callback:", result)
-      )
+      .on("liqpay.callback", (result) => {
+        console.log("liqpay.callback:", result);
+        if (result.result === "ok") {
+          setIsButtonShown(true);
+        }
+      })
       .on("liqpay.ready", () => {
         console.log("LiqPay ready");
         setLoadingState((prev) => ({ ...prev, loadingLiqPay: false }));
@@ -74,7 +80,32 @@ const LiqPayService = ({ setLoadingState }) => {
       abortController.abort();
     };
   }, []);
-  return <div id="liqpay-embed-container"></div>;
+  return (
+    <>
+      <div id="liqpay-embed-container"></div>
+      {isButtonShown && (
+        <div className="liqpay-go-to-product-box">
+          <Link
+            href={{
+              pathname: "/collection",
+              query: {
+                page: 1,
+                limit: 10,
+                category: "",
+                subCategory: "",
+                priceFrom: "",
+                priceTo: "",
+                sort: "date_new",
+                search: "",
+              },
+            }}
+          >
+            <button>Go To Product Page</button>
+          </Link>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default LiqPayService;
